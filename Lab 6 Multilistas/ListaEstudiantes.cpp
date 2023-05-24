96e7dd816007aa04c85518beb43a59d81d25bf91
Natalia D <natystra32@gmail.com>
2023-05-24 14:52:17 -0400

    Agregar archivos de proyecto.

---
+++ Lab 6 Multilistas/ListaEstudiantes.cpp
@@ -0,0 +1,152 @@
+#include "ListaEstudiantes.h"
+#include "Lista.cpp"
+using std::cout;
+
+void ListaEstudiantes::Inscribir_estudiante(NodoAsignatura* a, NodoEstudiante* e) {
+    if (a == nullptr) {
+        Lista::error();
+    }
+    else {
+        if (a->getPrimerEstudiante() == NULL) {
+            e->siguiente = a->getPrimerEstudiante();
+            a->pEstudiante = e;
+        }
+        else {
+            a->pEstudiante->anterior = e;
+            e->siguiente = a->getPrimerEstudiante();
+            a->pEstudiante = e;
+        }
+    }
+    return;
+}
+
+Nodo* ListaEstudiantes::getAnteriorValor(Nodo* p) {
+    return p->getAnteriorNodo();
+}
+void ListaEstudiantes::Mostrar_todos_estudiantes() {
+    if (esta_vacia()) {
+        cout << "La lista esta vacia" << "\n";
+        getchar();
+        return;
+    }
+    else {
+        NodoEstudiante* traveser;
+        traveser = (NodoEstudiante*)primero;
+        while (traveser != NULL) {
+            traveser = traveser->getSiguienteNodo();
+        }
+    }
+}
+
+int ListaEstudiantes::Cantidad_estudiantes(NodoAsignatura* a) {
+    int Cantidad = 0;
+    if (a == nullptr) {
+        Cantidad = 0;
+    }
+    else {
+        NodoEstudiante* traveser;
+        traveser = a->getPrimerEstudiante();
+        while (traveser != NULL) {
+            Cantidad++;
+            traveser = traveser->getSiguienteNodo();
+        }
+    }
+
+    return Cantidad;
+}
+
+NodoEstudiante* ListaEstudiantes::resultado_estudiante(string cod, NodoAsignatura* a) {
+    NodoEstudiante* traveser;
+    traveser = a->getPrimerEstudiante();
+    while (traveser != NULL) {
+        if (cod == traveser->getCodigo()) {
+            return traveser;
+        }
+        traveser = traveser->getSiguienteNodo();
+    }
+    return nullptr;
+}
+
+void ListaEstudiantes::BuscarEstudiante(string cod, NodoAsignatura* a) {
+    NodoEstudiante* est = resultado_estudiante(cod, a);
+
+    if (est == nullptr || a == nullptr) {
+        Lista::error();
+    }
+    else {
+        mostrar_un_estudiante(est);
+    }
+}
+
+void ListaEstudiantes::Eliminar_estudiante(string cod, NodoAsignatura* a) {
+    NodoEstudiante* est = resultado_estudiante(cod, a);
+    NodoEstudiante* izq;
+    NodoEstudiante* der;
+    der = 0;
+    if (est == nullptr || a == nullptr) {
+        Lista::error();
+    }
+    else {
+        if (est == a->getPrimerEstudiante()) {
+            Lista::iniciar();
+        }
+        else {
+            est->anterior = izq;
+            est->siguiente = der;
+            izq->siguiente = der;
+            der->anterior = izq;
+            delete est;
+        }
+    }
+}
+void ListaEstudiantes::Mostrar_anterior(string cod, NodoAsignatura* a) {
+
+    NodoEstudiante* est = resultado_estudiante(cod, a);
+    if (est == nullptr || a == nullptr) {
+        Lista::error();
+    }
+    else {
+        if (est == a->getPrimerEstudiante()) {
+            Lista::error();
+        }
+        else {
+            est = est->getAnteriorNodo();
+            Lista::mostrar_un_estudiante(est);
+        }
+    }
+}
+
+int ListaEstudiantes::Cantidad_genero(NodoAsignatura* a, char g) {
+    NodoEstudiante* est = a->getPrimerEstudiante();
+    int cantidad = 0;
+    if (a == nullptr || a == NULL) {
+        Lista::error();
+    }
+    else {
+        switch (g) {
+        case 'f': case 'F':
+            do {
+                if (est->getGenero() == Femenino) {
+                    cantidad++;
+                }
+                est = est->getSiguienteNodo();
+            } while (est != NULL);
+            break;
+        case 'm': case 'M':
+            do {
+                if (est->getGenero() == Masculino) {
+                    cantidad++;
+                }
+                est = est->getSiguienteNodo();
+            } while (est != NULL);
+            break;
+        default:
+            break;
+        }
+    }
+    return cantidad;
+}
+
+
+
+
